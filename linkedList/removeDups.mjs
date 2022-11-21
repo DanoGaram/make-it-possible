d851ade9e937217d4de6ce3f6be350151a00982e
Daniel Garcia Ramirez <daniel@MacBook-Pro-de-Daniel.local>
2022-11-21 09:55:21 -0600

    Add my first problems solved

---
+++ linkedList/removeDups.mjs
@@ -0,0 +1,43 @@
+import { LinkedList, Node } from "./linkedList.mjs";
+
+function deleteAll(node, data) {
+    let n = node;
+    while(n){
+        if(n.next && n.next.data === data){
+            n.next = n.next.next;
+        }
+        n = n.next;
+    }
+}
+
+function removeDups(ll) {
+    let n = ll.head;
+    while(n) {
+        deleteAll(n, n.data);
+        n = n.next;
+    }
+}
+
+function removeDupsV2(ll) {
+    const dic = {};
+    let n = ll.head;
+    while(n) {
+        if(n.next && dic[n.data]){
+            n.next = n.next.next;
+        } else {
+            dic[n.data] = true;
+        }
+        n = n.next;
+    }
+}
+
+const ll = new LinkedList(new Node(5));
+ll.appendToTail(2);
+ll.appendToTail(3);
+ll.appendToTail(5);
+ll.appendToTail(7);
+ll.appendToTail(3);
+ll.appendToTail(2);
+ll.print();
+removeDupsV2(ll);
+ll.print()
